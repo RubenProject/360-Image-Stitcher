9609bfae0a65a1f383f0b2a96cab7c4ee2d23e60
rubenproject <r.vdwaal1995@gmail.com>
2017-07-13 20:34:36 +0200

    implementated manual positioning interface

--- src/stitcher.cpp
+++ src/stitcher.cpp
@@ -5,8 +5,20 @@
 #include <stdio.h>
 #include <cstdlib>
 #include <ctime>
-#include <math.h>
+#include <cmath>
 #include <climits>
+
+#include <linux/input.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <termios.h>
+#include <signal.h>
+#include <unistd.h>
 
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
@@ -14,6 +26,7 @@
 #include <opencv2/calib3d.hpp>
 #include <opencv2/features2d.hpp>
 #include <opencv2/xfeatures2d.hpp>
+
 
 using namespace std;
 using namespace cv;
@@ -25,6 +38,16 @@ using namespace cv::xfeatures2d;
 #define THREAD_COUNT 12
 #define PI 3.1415926535897
 
+#define INPUT_QUEUE "/dev/input/event2"
+#define EVENT_LEN 16
+
+struct Orientation {
+    int x, y;
+    double r, b, s;
+};
+
+bool MANUAL_POSITIONING_DONE;
+Mat preview;
 
 
 static void help(char* progName)
@@ -183,7 +206,8 @@ Mat extractDescriptors(const Mat imgA, const Mat imgB)
     }
 
     //Display
-    drawMatches(imgA, keypointsA, imgB, keypointsB, good_matches, imgMatch, Scalar::all(-1), Scalar::all(-1),
+    drawMatches(imgA, keypointsA, imgB, keypointsB, good_matches, imgMatch,
+               Scalar::all(-1), Scalar::all(-1),
                vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
     namedWindow("matches", WINDOW_NORMAL);
     resizeWindow("matches", 600, 600);
@@ -231,7 +255,7 @@ void bundleAdjustment(Mat& src, double factor){
 }
 
 
-void joinImgs2(Mat A, Mat B, Mat& out, int overlap, int allign){
+void joinImgs(Mat A, Mat B, Mat& out, int overlap, int allign){
     out.create(A.rows, A.cols*2 - overlap, A.type());
     for (int i = 0; i < out.cols; i++){
         for (int j = 0; j < out.rows; j++){
@@ -255,6 +279,146 @@ Mat rotateImage(Mat src, double angle){
     warpAffine(src, res, r, res.size());
     return res;
 }
+
+
+
+
+void adjustImg(Mat A, Mat B, Orientation o_A, Orientation o_B, Mat& out){
+    if(o_A.b != 1.00)
+        bundleAdjustment(A, o_A.b);
+    if(o_B.b != 1.00)
+        bundleAdjustment(B, o_B.b);
+    if(o_A.r != 0.00)
+        A = rotateImage(A, o_A.r);
+    if(o_B.r != 0.00)
+        B = rotateImage(B, o_B.r);
+    joinImgs(B, A, out, o_A.x, o_A.y);
+    Mat A2 = out(Rect(0, 0, out.cols/2, out.rows));
+    Mat B2 = out(Rect(out.cols/2, 0, out.cols/2, out.rows));
+    joinImgs(B2, A2, out, o_B.x, o_B.y);
+    correctShift(out);
+} 
+
+
+void readInputEvent(int fd, int& key){
+    int rd, value, size = sizeof(struct input_event);
+    struct input_event ev[64];
+    while(true){
+        if ((rd = read(fd, ev, size * 64)) < size)
+            exit(0);
+
+        value = ev[0].value;
+        
+        if (value != ' ' && ev[1].value == 1 && ev[1].type == 1){
+            key = ev[1].code;
+            cout << ev[1].code << endl;;
+            break;
+        }
+    }
+}
+
+
+void parseInputPhysical(int key, Orientation& o_A, Orientation& o_B){
+    if(key == 17) o_A.y += 1; //w
+    else if(key == 30) o_A.x -= 1; //a
+    else if(key == 31) o_A.y -= 1; //s
+    else if(key == 32) o_A.x += 1; //d
+    else if(key == 23) o_B.y += 1; //i
+    else if(key == 36) o_B.x -= 1; //j
+    else if(key == 37) o_B.y -= 1; //k
+    else if(key == 38) o_B.x += 1; //l
+    else if(key == 16) o_A.r -= 0.05; //q
+    else if(key == 18) o_A.r += 0.05; //e
+    else if(key == 22) o_B.r -= 0.05; //u
+    else if(key == 24) o_B.r += 0.05; //o
+    else if(key == 44) o_A.s -= 0.01; //z
+    else if(key == 45) o_A.s += 0.01; //x
+    else if(key == 49) o_B.s -= 0.01; //n
+    else if(key == 50) o_B.s += 0.01; //m
+    else if(key == 46) o_A.b -= 0.01; //c
+    else if(key == 47) o_A.b += 0.01; //v
+    else if(key == 51) o_B.b -= 0.01; //,
+    else if(key == 52) o_B.b += 0.01; //.
+    else if(key == 28) MANUAL_POSITIONING_DONE = true; //enter
+    else cerr << "incorrect input!" << endl;
+}
+
+
+void parseInputASCII(int key, Orientation& o_A, Orientation& o_B){
+    if(key == (int)'w') o_A.y += 1; //w
+    else if(key == (int)'a') o_A.x -= 1; //a
+    else if(key == (int)'s') o_A.y -= 1; //s
+    else if(key == (int)'d') o_A.x += 1; //d
+    else if(key == (int)'i') o_B.y += 1; //i
+    else if(key == (int)'j') o_B.x -= 1; //j
+    else if(key == (int)'k') o_B.y -= 1; //k
+    else if(key == (int)'l') o_B.x += 1; //l
+    else if(key == (int)'q') o_A.r -= 0.05; //q
+    else if(key == (int)'e') o_A.r += 0.05; //e
+    else if(key == (int)'u') o_B.r -= 0.05; //u
+    else if(key == (int)'o') o_B.r += 0.05; //o
+    else if(key == (int)'z') o_A.s -= 0.01; //z
+    else if(key == (int)'x') o_A.s += 0.01; //x
+    else if(key == (int)'n') o_B.s -= 0.01; //n
+    else if(key == (int)'m') o_B.s += 0.01; //m
+    else if(key == (int)'c') o_A.b -= 0.01; //c
+    else if(key == (int)'v') o_A.b += 0.01; //v
+    else if(key == (int)',') o_B.b -= 0.01; //,
+    else if(key == (int)'.') o_B.b += 0.01; //.
+    else if(key == 13) MANUAL_POSITIONING_DONE = true; //enter
+    else cerr << "incorrect input!" << endl;
+}
+
+
+void displayPreview(){
+    namedWindow("Manual Position" , WINDOW_NORMAL);
+    resizeWindow("Manual Position" , 1200, 600);
+    while(!MANUAL_POSITIONING_DONE){
+        imshow("Manual Position", preview);
+        waitKey(30);
+    }
+}
+
+void interactImg(const Mat A, const Mat B, Orientation& o_A, Orientation& o_B){
+    //Initial orientation
+    o_A = {.x = 0, .y = 0,
+            .r = 0.00,
+            .b = 1.00,
+            .s = 1.00};
+    o_B = {.x = 0, .y = 0,
+            .r = 0.00,
+            .b = 1.00,
+            .s = 1.00};
+
+    int fd, key;
+    char name[256];
+
+    //downscaling for faster operations
+    Mat A_s, B_s, out_s;
+    resize(A, A_s, Size(A.cols/8, A.rows/8));
+    resize(B, B_s, Size(B.cols/8., B.rows/8));
+
+    if((fd = open(INPUT_QUEUE, O_RDONLY)) == -1)
+        cerr << "cannot read from device" << endl;
+
+    ioctl (fd, EVIOCGNAME (sizeof (name)), name);
+    cerr << "reading from: " << name << endl;
+
+    MANUAL_POSITIONING_DONE = false;
+    adjustImg(A_s, B_s, o_A, o_B, out_s);
+    preview = out_s;
+    thread t = thread(displayPreview);
+
+    while(!MANUAL_POSITIONING_DONE){
+        readInputEvent(fd, key);
+        parseInputPhysical(key, o_A, o_B);
+        adjustImg(A_s, B_s, o_A, o_B, out_s);
+        preview = out_s;
+        cout << key << endl;
+    }
+    cout << "finalizing positioning" << endl;
+}
+
 
 int main( int argc, char* argv[])
 {
@@ -311,42 +475,9 @@ int main( int argc, char* argv[])
     //warpPerspective(dst1, res1, h1, res1.size());
     //hconcat(dst1, dst0, out);
 
-    //namedWindow("res0", WINDOW_NORMAL);
-    //resizeWindow("res0", 600, 600);
-    //imshow("res0", res0);
-    //namedWindow("res1", WINDOW_NORMAL);
-    //resizeWindow("res1", 600, 600);
-    //imshow("res1", res1);
-    //waitKey(0);
-    //joinImgs(res0, res1, out);
-
-
-    //hconcat(dst0, dst1, out);
-    int overlap = 3677;
-    int allign = 0;
-    int x, y;
-    bundleAdjustment(dst0, 0.96);
-    bundleAdjustment(dst1, 0.96);
-    dst0 = rotateImage(dst0, -0.73);
-    while (true){
-        joinImgs2(dst1, dst0, out, overlap, allign);
-        Mat A2 = out(Rect(0, 0, out.cols/2, out.rows));
-        Mat B2 = out(Rect(out.cols/2, 0, out.cols/2, out.rows));
-        joinImgs2(B2, A2, out, overlap, 0);
-        correctShift(out);
-        string windowvalue = "x = ";
-        windowvalue += to_string(overlap);
-        windowvalue += " ,y = ";
-        windowvalue += to_string(allign);
-        namedWindow(windowvalue, WINDOW_NORMAL);
-        resizeWindow(windowvalue, 1200, 600);
-        imshow(windowvalue, out);
-        waitKey(0);
-        cout << "x: , y: " << endl;
-        cin >> x >> y;
-        overlap += x;
-        allign += y;
-    }
+    Orientation o_A, o_B;
+    interactImg(dst0, dst1, o_A, o_B);
+    adjustImg(dst0, dst1, o_A, o_B, out);
 
     string outputFileName(filename);
     outputFileName = outputFileName.substr(outputFileName.length() - 12);
